9c5a4a4f9737230d4b95551b63ed71a2db6edeb2
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:08:23 -0500

    Added App
    This is the first version of Number Generator that GitHub has ever seen!

---
+++ Assets/Number.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var number : UnityEngine.UI.Text;
+var Before : UnityEngine.UI.InputField;
+var After : UnityEngine.UI.InputField;
+var dee : String;
+var deez : int;
+var deez1 : int;
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function Randomize () {
+	deez = int.Parse(Before.text);
+	var rnumber = Random.Range(int.Parse(Before.text),int.Parse(After.text) + 1);
+	number.text = rnumber.ToString();
+	Debug.Log (number);
+}
